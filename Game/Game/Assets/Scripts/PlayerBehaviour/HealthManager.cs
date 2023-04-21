71dd33f45e43dbccb86dec443b1cc39a5376175e
Segoul01 <segoul01@gmail.com>
2023-04-21 19:19:11 +0530

    Flash Red Thingy

--- Game/Game/Assets/Scripts/PlayerBehaviour/HealthManager.cs
+++ Game/Game/Assets/Scripts/PlayerBehaviour/HealthManager.cs
@@ -1,17 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    [SerializeField] private float m_Health = 5;
-    
+    [SerializeField] private float health = 5f;
+    [SerializeField] private float flashLength = 0.2f;
+
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Color flashColor;
+    [SerializeField] private Color origColor;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void IsAttacked(float dmg)
     {
-        m_Health -= dmg;
+        health -= dmg;
         Debug.Log("Dealt Damage To Player!");
 
-        if (m_Health <= 0)
+        StartCoroutine(Flash());
+
+        if (health <= 0)
         {
             Debug.Log("Player Is Dead!, Game Over!!");
         }
     }
+
+    IEnumerator Flash()
+    {
+        ChangeSpriteColor(flashColor);
+
+        yield return new WaitForSeconds(flashLength);
+
+        ChangeSpriteColor(origColor);
+    }
+
+    private void ChangeSpriteColor(Color color)
+    {
+        spriteRenderer.color = color;
+    }    
 }
